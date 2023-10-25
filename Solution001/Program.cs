d3d930bf13d288fb0a230198ffebb5fc89c7b269
Cézanne <devcez82@gmail.com>
2023-10-25 16:33:02 +0900

    [DAY001] 짝수와 홀수
    - 프로그래머스 Lv.0

---
+++ Solution001/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 코드를 작성하시오.
+            string input = Console.ReadLine();
+            int num = 0;
+            string answer = "";
+
+            if(int.TryParse(input, out num))
+            {
+                if(num % 2 == 0) answer = "Even";
+                else answer = "Odd";
+
+                Console.WriteLine(answer);
+            }
+        }
+    }
+}
