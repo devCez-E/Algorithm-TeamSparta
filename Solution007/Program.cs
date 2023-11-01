6c046e33fc6cab9964ca601d0450fd6eb939c1f2
Cézanne <devcez82@gmail.com>
2023-11-01 09:35:04 +0900

    [DAY007] 자연수를 뒤집어 배열로 만들기

---
+++ Solution007/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Solution007 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        // 자연수 X를 뒤집어 각 자리 숫자를 원소로 가지는 배열
+        static void Main(string[] args)
+        {
+            Program pg = new Program();
+            var answer1 = pg.Solution(12345);
+
+            Console.WriteLine($"12345를 뒤집어 각 자리 숫자를 원소로 가지는 배열 : {answer1}");
+        }
+
+        int[] Solution(long n){
+            int size = (int)Math.Log10(n) + 1;
+            int[] arr = new int[size];
+
+            for(int i = 0; i < size; i++){
+                arr[i] = (int)(n % 10);
+                n /= 10;
+            }
+
+            return arr;
+        }
+    }
+}
