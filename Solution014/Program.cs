5ae2d670dbaeb0773de00e9b50ff0e06815f8e35
Cézanne <devcez82@gmail.com>
2023-11-08 20:54:37 +0900

    [DAY014] 서울에서 김서방 찾기

---
+++ Solution014/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Solution014 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        // 서울에서 김서방 찾기
+        // String 배열 L 에서 문자열 S("Kim")의 위치를 찾아 반환하세요.
+        static void Main(string[] args)
+        {
+            string[] arr = {"Jane", "Kim"};
+
+            Program pg = new Program();
+            var answer0 = pg.Solution(arr);
+            Console.WriteLine($"김서방은 {answer0}에 있다.");
+        }
+    
+        int Solution(string[] L)
+        {
+            return Array.IndexOf(L, "Kim");
+        }
+    }
+}
