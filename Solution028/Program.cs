617ccb88ee2b1b28f98cdcd9104313a4307baae7
Cézanne <devcez82@gmail.com>
2023-11-17 09:42:28 +0900

    [DAY028] 직사각형 별찍기

---
+++ Solution028/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Solution028 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        // 가로의 길이가 n, 세로의 길이가 m인 직사각형을 출력하시오
+        static void Main(string[] args)
+        {
+            Program pg = new Program();
+            String[] s;
+
+            Console.Clear();
+            s = Console.ReadLine().Split(' ');
+
+            int a = Int32.Parse(s[0]);
+            int b = Int32.Parse(s[1]);
+
+            pg.PrintRectangle(a, b);
+        }
+
+        void PrintRectangle(int n, int m){
+            for(int i = 0; i < m; i++){
+                for(int j = 0; j < n; j++){
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
