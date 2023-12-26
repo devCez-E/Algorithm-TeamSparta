22b2b02139dd324900e4d9fef5f049f4b622fee4
leesungeon <devcez82@gmail.com>
2023-12-26 10:08:14 +0900

    [DAY33] 이상한 문자 만들기

---
+++ Lv01/Solution033/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace Solution033
+{
+    class Program
+    {
+        //문자열 s는 한 개 이상의 단어로 구성되어 있습니다.
+        //각 단어는 하나 이상의 공백문자로 구분되어 있습니다.
+        //각 단어의 짝수번째 알파벳은 대문자로, 홀수번째 알파벳은 소문자로 바꾼 문자열을 반환하시오.
+        static void Main(string[] args)
+        {
+            Program pg = new Program();
+            var answer1 = pg.Solution("try hello world");  // "TrY HeLlO WoRlD"
+
+            Console.WriteLine($"try hello world의 이상한 문자열 : {answer1}");
+        }
+
+        string Solution(string s){
+            bool isEven = true;
+            StringBuilder sb = new StringBuilder();
+
+            for(int i = 0; i < s.Length; i++){
+                char c = s[i];
+                if(c != ' '){
+                    if(isEven)
+                    {
+                        sb.Append(Char.ToUpper(s[i]));
+                    }
+                    else
+                    {
+                        sb.Append(s[i]);
+                    }
+                    isEven=!isEven;
+                }
+                else
+                {
+                    isEven=true;
+                    sb.Append(" ");
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
